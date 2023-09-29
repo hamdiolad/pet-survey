574ec26e88e8c279ded56862ba2c260b798ff59c
So Youn Choi <manghatuhyo@gmail.com>
2023-09-29 10:21:50 +0100

    exemple of redex at namepet

--- src/components/NamePet.jsx
+++ src/components/NamePet.jsx
@@ -2,13 +2,22 @@ import { useState } from 'react';
 
 const NamePet = ({ onNext, setNamePetProp }) => {
   const [namePet, setNamePet] = useState('');
+  const [isValid, setIsValid] = useState(true);
 
   const handleNamePet = (event) => {
     setNamePet(event.target.value);
+
+    const regex = /^[a-zA-Z]{2,20}$/;
+    setIsValid(regex.test(event.target.value));
   };
 
   const handleSubmit = (event) => {
     event.preventDefault();
+
+    if (!isValid) {
+      return; 
+    }
+
     console.log(namePet);
     setNamePetProp(namePet);
     onNext();
@@ -23,6 +32,7 @@ const NamePet = ({ onNext, setNamePetProp }) => {
         style={{ width: '210px' }}
         required
       />
+       {!isValid && <p style={{color: 'blue'}}>Pet name should be 2-20 alphabets long.</p>}
       <button type="submit" className="next-button">Next</button>
     </form>
   );
