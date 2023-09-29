ba87a8b30eede4d015a74fb815f043731e2ad30a
So Youn Choi <manghatuhyo@gmail.com>
2023-09-29 10:45:21 +0100

    added regex to yourname, yourage

--- src/components/YourAge.jsx
+++ src/components/YourAge.jsx
@@ -2,13 +2,21 @@ import { useState } from 'react';
 
 const YourAge = ({ onNext, setYourAgeProp }) => {
     const [yourAge, setYourAge] = useState('');
+    const [isValid, setIsValid] = useState(true);
 
     const handleYourAge = (event) => {
         setYourAge(event.target.value);
+
+        const regex = /^0$|^[1-9][0-9]*$/;  // all digit including 0
+        setIsValid(regex.test(event.target.value));
     };
 
     const handleSubmit = (event) => {
         event.preventDefault();
+
+        if (!isValid) {
+            return; 
+        }
         console.log(yourAge);
         setYourAgeProp(yourAge);
         onNext();
@@ -23,6 +31,8 @@ const YourAge = ({ onNext, setYourAgeProp }) => {
                 style={{ width: '210px' }}
                 required
             />
+            {!isValid && <p style={{color: 'blue'}}>Please enter a valid age (only number).</p>}
+
             <button type="submit" className="next-button">Next</button>
         </form>
     );
--- src/components/YourName.jsx
+++ src/components/YourName.jsx
@@ -2,13 +2,21 @@ import { useState } from 'react';
 
 const YourName = ({ onNext, setYourNameProp }) => {
     const [yourName, setYourName] = useState('');
+    const [isValid, setIsValid] = useState(true);
 
     const handleYourName = (event) => {
         setYourName(event.target.value);
+
+        const regex = /^[a-zA-Z]{2,20}$/;
+        setIsValid(regex.test(event.target.value));
     };
 
     const handleSubmit = (event) => {
         event.preventDefault();
+
+        if (!isValid) {
+            return; 
+        }
         console.log(yourName);
         setYourNameProp(yourName);
         onNext();
@@ -23,6 +31,7 @@ const YourName = ({ onNext, setYourNameProp }) => {
                 style={{ width: '210px' }}
                 required
             />
+            {!isValid && <p style={{color: 'blue'}}>Your name should be 2-20 alphabets long.</p>}
 
             <button type="submit" className="next-button">Next</button>
         </form>
